731735473fa90ca5cd43c1e5caa86dadd3a570f6
Sarthak Khandelwal <55028063+HellVolhard07@users.noreply.github.com>
2022-01-09 19:55:43 +0530

    Time: 6 ms (8.33%), Space: 6 MB (53.15%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int count[46] = {0};
+    int climbStairs(int n) {
+        if(n == 0 or n == 1 or n == 2) {
+            return n;
+        }
+        if(count[n] != 0) {
+            return count[n];
+        }
+        return count[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
