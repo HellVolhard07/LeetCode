6cec465a8115f1646178a3be47f9f9e113e94632
Sarthak Khandelwal <55028063+HellVolhard07@users.noreply.github.com>
2022-02-10 20:13:30 +0530

    Time: 93 ms (64.80%), Space: 37 MB (53.93%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    vector<int> getPrefixSum(vector<int> &nums) {
+        int n = nums.size();
+        vector<int> prefix(n);
+        prefix[0] = nums[0];
+        for(int i = 1 ; i < n ; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+        return prefix;
+    }
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> prefix = getPrefixSum(nums);
+        unordered_map<int, int> freq;
+        int ans = 0;
+        for(int i : prefix) {
+            if(i == k) {
+                ans++;
+            }
+            if(freq.count(i - k) > 0) {
+                ans += freq[i - k];
+            }
+            freq[i]++;
+        }
+        return ans;
+    }
+};
