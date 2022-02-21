68f6a3df070a6551310f826ca6859df06a5f5c4c
Sarthak Khandelwal <55028063+HellVolhard07@users.noreply.github.com>
2022-02-21 17:36:17 +0530

    Time: 31 ms (39.03%), Space: 19.5 MB (99.35%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,30 @@
+// MOORE'S VOTING ALGORITHM
+// Time: O(n), Space: O(1)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int counter = 0;
+        int ans;
+        for(int num : nums) {
+            if(counter == 0) {
+                ans = num;
+            }
+            counter += num == ans ? 1 : -1;
+            // if(num == ans) {
+            //     counter++;
+            // }
+            // else {
+            //     counter--;
+            // }
+        }
+        return ans;
+    }
+};
+
+/*
+Time: O(n), Space: O(n)
+int majorityElement(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int, int> freq;
         int ans = 0;
@@ -15,4 +39,5 @@ public:
         }
         return ans;
     }
-};
+
+*/
